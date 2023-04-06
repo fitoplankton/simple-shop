c5154480c61d71999df3c98f93572bc478833caf
fitoplankton <71702064+fitoplankton@users.noreply.github.com>
2023-04-06 16:37:31 +0700

    intial commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return view('product.index',compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('product.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // dd($request->all());
+        // $data = $request->all();
+        // $product = Product::create($data);
+
+        $file = $request->file('photo');
+        $filename = time() . '.' .$file->getClientOriginalExtension();
+        $path = $request->file('photo')->storeAs('public/products', $filename);
+
+        // dd($path);
+        $path = str_replace('public/','',$path);
+
+        $data = [
+            'name' => $request->name,
+            'price' => $request->price,
+            'stocks' => $request->stocks,
+            'photo' => $path
+        ];
+
+        $product = Product::create($data);
+
+        return redirect()->route('product.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return view('product.update', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $file = $request->file('photo');
+        $filename = time() . '.' .$file->getClientOriginalExtension();
+        $path = $request->file('photo')->storeAs('public/products', $filename);
+
+        // dd($path);
+        $path = str_replace('public/','',$path);
+
+        $product = Product::find($id);
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+        $product->stocks = $request->stocks;
+        $product->photo = $path;
+        $product->save();
+
+        return redirect()->route('product.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+       
+        try {
+            // menghapus foto
+            Storage::delete('public/'. $product->photo);
+
+            $product->delete();
+
+        } catch(\Throwable $th){
+            //throw $th;
+        }
+
+        $product->delete();
+
+        return redirect()->route('product.index');
+    }
+}
---
+++ resources/views/product/update.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('title', 'Product Index Page')
+
+@section('scripts')
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.css" />
+
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.js"></script>
+@endsection
+
+@section('content')
+    <h5 class="mb-4">Edit Product</h5>
+    <form action="{{ route('product.update', $product->id) }}" method="post" enctype="multipart/form-data">
+        @method('PUT')
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Nama</label>
+            <input type="text" name="name" class="form-control" id="name" value="{{ $product->name }}">
+        </div>
+        <div class="mb-3">
+            <label for="price" class="form-label">Harga</label>
+            <input type="number" name="price" class="form-control" id="price" value="{{ $product->price }}">
+        </div>
+        <div class="mb-3">
+            <label for="stocks" class="form-label">Stock</label>
+            <input type="number" name="stocks" class="form-control" id="stocks" value="{{ $product->stocks }}">
+        </div>
+        <div class="mb-3">
+            <label for="photo">Upload Gambar</label>
+            <input type="file" name="photo" class="form-control" id="photo">
+        </div>
+        @if ($product->photo != null)
+            <div style="width:200px">
+                <img src="{{ asset('storage/' . $product->photo) }}" class="img-fluid" alt="...">
+
+            </div>
+        @else
+            <p class="text-info">tidak ada foto</p>
+        @endif
+
+        <div class="d-flex">
+            <button type="submit" class="btn btn-primary me-3">Simpan</button>
+            <a href="{{ route('product.index') }}" type="button" class="btn btn-danger">Batal</a>
+        </div>
+    </form>
+@endsection
+
+@section('js')
+    <script>
+        $(document).ready(function() {
+            $('#exampleTable').DataTable();
+        });
+    </script>
+@endsection
